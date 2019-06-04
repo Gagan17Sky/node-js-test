39151b5db16dbd70f654ee2ddc6c3e054ef34169
Gagandeep Singh <gagandeepsingh.in@gmail.com>
2019-06-04 07:45:23 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page with hello. */
-router.get('/hello/:name/:occupation', function(req, res, next) {
-  res.render('hello', { title: 'Express', name: req.params.name, occupation: req.params.occupation });
-});
-
 module.exports = router;
